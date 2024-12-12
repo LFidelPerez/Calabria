f1799c8906f4ea08172d616044acad4b4ba2cf50
Leonardo Fidel Perez <leonardo.fidel.perez@gmail.com>
2024-12-12 12:29:45 -0300

    Changed stock main form location

---
+++ Calabria/Stock/frmStockMain.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Calabria
+{
+	partial class frmStockMain
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(190, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(135, 39);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "STOCK";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(26, 89);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(111, 43);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Items";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// frmStock
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(553, 198);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label1);
+			this.Name = "frmStock";
+			this.Text = "frmStock";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+	}
+}
---
+++ Calabria/Stock/frmStockMain.cs
@@ -0,0 +1,22 @@
+﻿using Calabria.Base.Forms;
+using Calabria.Stock;
+using System;
+
+namespace Calabria
+{
+	public partial class frmStockMain : BaseForm
+	{
+		private frmStockItemList _frmStockItemList;
+
+		public frmStockMain()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			_frmStockItemList = new frmStockItemList();
+			_frmStockItemList.ShowDialog();
+		}
+	}
+}
