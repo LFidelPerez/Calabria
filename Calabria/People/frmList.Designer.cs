45730571de8f545f66f32b87c97e3321614ed6aa
marcmax <marquitoevaristo@gmail.com>
2025-03-17 22:24:17 -0300

    se agregó el archivo eliminados.cs y se actualizó el frmList.cs para acceder al mismo

--- Calabria/People/frmList.Designer.cs
+++ Calabria/People/frmList.Designer.cs
@@ -28,67 +28,72 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.txt_search = new System.Windows.Forms.TextBox();
-			this.lbl_search = new System.Windows.Forms.Label();
-			this.btn_add = new System.Windows.Forms.Button();
-			this.btnClose = new System.Windows.Forms.Button();
-			this.dgvList = new System.Windows.Forms.DataGridView();
-			this.label1 = new System.Windows.Forms.Label();
-			this.btn_Search = new System.Windows.Forms.Button();
-			this.ItemId = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.Names = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.Surnames = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.Age = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.Contacto = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.emergencyContact = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.isMember = new System.Windows.Forms.DataGridViewCheckBoxColumn();
-			((System.ComponentModel.ISupportInitialize)(this.dgvList)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// txt_search
-			// 
-			this.txt_search.Location = new System.Drawing.Point(158, 68);
-			this.txt_search.Name = "txt_search";
-			this.txt_search.Size = new System.Drawing.Size(242, 22);
-			this.txt_search.TabIndex = 0;
-			this.txt_search.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txt_search_KeyUp);
-			// 
-			// lbl_search
-			// 
-			this.lbl_search.AutoSize = true;
-			this.lbl_search.Location = new System.Drawing.Point(100, 71);
-			this.lbl_search.Name = "lbl_search";
-			this.lbl_search.Size = new System.Drawing.Size(52, 16);
-			this.lbl_search.TabIndex = 11;
-			this.lbl_search.Text = "Buscar:";
-			// 
-			// btn_add
-			// 
-			this.btn_add.Location = new System.Drawing.Point(12, 64);
-			this.btn_add.Name = "btn_add";
-			this.btn_add.Size = new System.Drawing.Size(75, 31);
-			this.btn_add.TabIndex = 10;
-			this.btn_add.Text = "Agregar";
-			this.btn_add.UseVisualStyleBackColor = true;
-			this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
-			// 
-			// btnClose
-			// 
-			this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.btnClose.Location = new System.Drawing.Point(978, 12);
-			this.btnClose.Name = "btnClose";
-			this.btnClose.Size = new System.Drawing.Size(117, 56);
-			this.btnClose.TabIndex = 9;
-			this.btnClose.Text = "Cerrar";
-			this.btnClose.UseVisualStyleBackColor = true;
-			this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
-			// 
-			// dgvList
-			// 
-			this.dgvList.AllowUserToAddRows = false;
-			this.dgvList.AllowUserToDeleteRows = false;
-			this.dgvList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.dgvList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.txt_search = new System.Windows.Forms.TextBox();
+            this.lbl_search = new System.Windows.Forms.Label();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.dgvList = new System.Windows.Forms.DataGridView();
+            this.ItemId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Names = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Surnames = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Age = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Contacto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.emergencyContact = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.isMember = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Search = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_search
+            // 
+            this.txt_search.Location = new System.Drawing.Point(118, 55);
+            this.txt_search.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_search.Name = "txt_search";
+            this.txt_search.Size = new System.Drawing.Size(182, 20);
+            this.txt_search.TabIndex = 0;
+            this.txt_search.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txt_search_KeyUp);
+            // 
+            // lbl_search
+            // 
+            this.lbl_search.AutoSize = true;
+            this.lbl_search.Location = new System.Drawing.Point(75, 58);
+            this.lbl_search.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_search.Name = "lbl_search";
+            this.lbl_search.Size = new System.Drawing.Size(43, 13);
+            this.lbl_search.TabIndex = 11;
+            this.lbl_search.Text = "Buscar:";
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(9, 52);
+            this.btn_add.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(56, 25);
+            this.btn_add.TabIndex = 10;
+            this.btn_add.Text = "Agregar";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClose.Location = new System.Drawing.Point(734, 10);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(88, 46);
+            this.btnClose.TabIndex = 9;
+            this.btnClose.Text = "Cerrar";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // dgvList
+            // 
+            this.dgvList.AllowUserToAddRows = false;
+            this.dgvList.AllowUserToDeleteRows = false;
+            this.dgvList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.ItemId,
             this.Names,
             this.Surnames,
@@ -96,116 +101,131 @@
             this.Contacto,
             this.emergencyContact,
             this.isMember});
-			this.dgvList.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
-			this.dgvList.Location = new System.Drawing.Point(12, 122);
-			this.dgvList.Name = "dgvList";
-			this.dgvList.ReadOnly = true;
-			this.dgvList.RowHeadersWidth = 51;
-			this.dgvList.RowTemplate.Height = 24;
-			this.dgvList.Size = new System.Drawing.Size(1083, 316);
-			this.dgvList.TabIndex = 8;
-			this.dgvList.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvList_CellContentClick);
-			this.dgvList.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvList_CellContentClick);
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(12, 9);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(289, 39);
-			this.label1.TabIndex = 7;
-			this.label1.Text = "Lista de Personas";
-			this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-			// 
-			// btn_Search
-			// 
-			this.btn_Search.Location = new System.Drawing.Point(406, 64);
-			this.btn_Search.Name = "btn_Search";
-			this.btn_Search.Size = new System.Drawing.Size(75, 31);
-			this.btn_Search.TabIndex = 13;
-			this.btn_Search.Text = "Buscar";
-			this.btn_Search.UseVisualStyleBackColor = true;
-			this.btn_Search.Click += new System.EventHandler(this.button1_Click);
-			// 
-			// ItemId
-			// 
-			this.ItemId.HeaderText = "Id";
-			this.ItemId.MinimumWidth = 6;
-			this.ItemId.Name = "ItemId";
-			this.ItemId.ReadOnly = true;
-			this.ItemId.Visible = false;
-			this.ItemId.Width = 125;
-			// 
-			// Names
-			// 
-			this.Names.HeaderText = "Nombres";
-			this.Names.MinimumWidth = 6;
-			this.Names.Name = "Names";
-			this.Names.ReadOnly = true;
-			this.Names.Width = 125;
-			// 
-			// Surnames
-			// 
-			this.Surnames.HeaderText = "Apellidos";
-			this.Surnames.MinimumWidth = 6;
-			this.Surnames.Name = "Surnames";
-			this.Surnames.ReadOnly = true;
-			this.Surnames.Width = 125;
-			// 
-			// Age
-			// 
-			this.Age.HeaderText = "Edad";
-			this.Age.MinimumWidth = 6;
-			this.Age.Name = "Age";
-			this.Age.ReadOnly = true;
-			this.Age.Width = 125;
-			// 
-			// Contacto
-			// 
-			this.Contacto.HeaderText = "Contact";
-			this.Contacto.MinimumWidth = 6;
-			this.Contacto.Name = "Contacto";
-			this.Contacto.ReadOnly = true;
-			this.Contacto.Width = 125;
-			// 
-			// emergencyContact
-			// 
-			this.emergencyContact.HeaderText = "Nº Emergencia";
-			this.emergencyContact.MinimumWidth = 6;
-			this.emergencyContact.Name = "emergencyContact";
-			this.emergencyContact.ReadOnly = true;
-			this.emergencyContact.Width = 125;
-			// 
-			// isMember
-			// 
-			this.isMember.HeaderText = "Socio";
-			this.isMember.MinimumWidth = 6;
-			this.isMember.Name = "isMember";
-			this.isMember.ReadOnly = true;
-			this.isMember.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-			this.isMember.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
-			this.isMember.Width = 125;
-			// 
-			// frmList
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.CancelButton = this.btnClose;
-			this.ClientSize = new System.Drawing.Size(1119, 450);
-			this.Controls.Add(this.btn_Search);
-			this.Controls.Add(this.txt_search);
-			this.Controls.Add(this.lbl_search);
-			this.Controls.Add(this.btn_add);
-			this.Controls.Add(this.btnClose);
-			this.Controls.Add(this.dgvList);
-			this.Controls.Add(this.label1);
-			this.Name = "frmList";
-			this.Text = "frmList";
-			this.Load += new System.EventHandler(this.frmList_Load);
-			((System.ComponentModel.ISupportInitialize)(this.dgvList)).EndInit();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.dgvList.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgvList.Location = new System.Drawing.Point(9, 99);
+            this.dgvList.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgvList.Name = "dgvList";
+            this.dgvList.ReadOnly = true;
+            this.dgvList.RowHeadersWidth = 51;
+            this.dgvList.RowTemplate.Height = 24;
+            this.dgvList.Size = new System.Drawing.Size(812, 257);
+            this.dgvList.TabIndex = 8;
+            this.dgvList.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvList_CellContentClick);
+            this.dgvList.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvList_CellContentClick);
+            // 
+            // ItemId
+            // 
+            this.ItemId.HeaderText = "Id";
+            this.ItemId.MinimumWidth = 6;
+            this.ItemId.Name = "ItemId";
+            this.ItemId.ReadOnly = true;
+            this.ItemId.Visible = false;
+            this.ItemId.Width = 125;
+            // 
+            // Names
+            // 
+            this.Names.HeaderText = "Nombres";
+            this.Names.MinimumWidth = 6;
+            this.Names.Name = "Names";
+            this.Names.ReadOnly = true;
+            this.Names.Width = 125;
+            // 
+            // Surnames
+            // 
+            this.Surnames.HeaderText = "Apellidos";
+            this.Surnames.MinimumWidth = 6;
+            this.Surnames.Name = "Surnames";
+            this.Surnames.ReadOnly = true;
+            this.Surnames.Width = 125;
+            // 
+            // Age
+            // 
+            this.Age.HeaderText = "Edad";
+            this.Age.MinimumWidth = 6;
+            this.Age.Name = "Age";
+            this.Age.ReadOnly = true;
+            this.Age.Width = 125;
+            // 
+            // Contacto
+            // 
+            this.Contacto.HeaderText = "Contact";
+            this.Contacto.MinimumWidth = 6;
+            this.Contacto.Name = "Contacto";
+            this.Contacto.ReadOnly = true;
+            this.Contacto.Width = 125;
+            // 
+            // emergencyContact
+            // 
+            this.emergencyContact.HeaderText = "Nº Emergencia";
+            this.emergencyContact.MinimumWidth = 6;
+            this.emergencyContact.Name = "emergencyContact";
+            this.emergencyContact.ReadOnly = true;
+            this.emergencyContact.Width = 125;
+            // 
+            // isMember
+            // 
+            this.isMember.HeaderText = "Socio";
+            this.isMember.MinimumWidth = 6;
+            this.isMember.Name = "isMember";
+            this.isMember.ReadOnly = true;
+            this.isMember.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.isMember.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.isMember.Width = 125;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 31);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Lista de Personas";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btn_Search
+            // 
+            this.btn_Search.Location = new System.Drawing.Point(304, 52);
+            this.btn_Search.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_Search.Name = "btn_Search";
+            this.btn_Search.Size = new System.Drawing.Size(56, 25);
+            this.btn_Search.TabIndex = 13;
+            this.btn_Search.Text = "Buscar";
+            this.btn_Search.UseVisualStyleBackColor = true;
+            this.btn_Search.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(428, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // frmList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnClose;
+            this.ClientSize = new System.Drawing.Size(839, 366);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_Search);
+            this.Controls.Add(this.txt_search);
+            this.Controls.Add(this.lbl_search);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.dgvList);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "frmList";
+            this.Text = "frmList";
+            this.Load += new System.EventHandler(this.frmList_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
@@ -225,5 +245,6 @@
 		private System.Windows.Forms.DataGridViewTextBoxColumn Contacto;
 		private System.Windows.Forms.DataGridViewTextBoxColumn emergencyContact;
 		private System.Windows.Forms.DataGridViewCheckBoxColumn isMember;
-	}
+        private System.Windows.Forms.Button button1;
+    }
 }
